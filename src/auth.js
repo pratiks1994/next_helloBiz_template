1e42e23d9f7cec8fa6e68f162492ffe6da0070aa
pratiks1994 <pratiksavaliya071994@gmail.com>
2025-02-04 13:21:08 +0530

    added duenade

--- src/auth.js
+++ src/auth.js
@@ -1,6 +1,14 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import DuendeIdentityServer6 from "@auth/core/providers/duende-identity-server6";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [GitHub],
+    providers: [
+        GitHub,
+        DuendeIdentityServer6({
+            clientId: "interactive.confidential",
+            clientSecret: "secret",
+            issuer: "https://demo.duendesoftware.com",
+        }),
+    ],
 });
